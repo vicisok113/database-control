eef587e8613fd234a4cff8c32464e94e376865eb
Tudosa Victor <tudosa.victor@yahoo.com>
2016-07-25 10:49:41 +0300

    database-control

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysql_connect("localhost","zaomedia","Par0L#1.23^%HHG");
+$conn = mysql_connect("localhost","root","");
 	if($selectdb = mysql_select_db('zaomedia_database_control')) {
 		$sql = "SET NAMES 'utf8'";
 		mysql_query($sql, $conn);
